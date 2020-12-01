68e1f9696c4421d9145f0b7f173ac56ad7b22373
Mathieu Derrit <mathieu.derrit@gmail.com>
2020-12-01 14:27:33 +0100

    Correction controller

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -14,24 +14,23 @@ class ProStagesController extends AbstractController
             'controller_name' => 'Bienvenue sur la page d\'accueil de Prostages',
         ]);
     }
-}
-public function entreprises(): Response
-{
-    return $this->render('pro_stages/entreprises.html.twig', [
-        'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
+    public function entreprises(): Response
+    {
+        return $this->render('pro_stages/entreprises.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
+        ]);
+    }
+    public function formations(): Response
+    {
+        return $this->render('pro_stages/formations.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des formations de l\'IUT',
+        ]);
+    }
+    public function stages(): Response
+    {
+        return $this->render('pro_stages/stages.html.twig', [
+            'controller_name' => "Cette page affichera le descriptif du stage ayant pour identifiant ".$id,
     ]);
+    }
 }
-public function formations(): Response
-{
-    return $this->render('pro_stages/formations.html.twig', [
-        'controller_name' => 'Cette page affichera la liste des formations de l\'IUT',
-    ]);
-}
-public function stages(): Response
-{
-    return $this->render('pro_stages/stages.html.twig', [
-        'controller_name' => "Cette page affichera le descriptif du stage ayant pour identifiant ".$id,
-    ]);
-}
-
 ?>
